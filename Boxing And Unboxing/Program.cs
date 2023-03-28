418db794878652dce4a16e3c9b737ab9a7ab018f
sezahedi <sezahedi13@gmail.com>
2023-03-29 01:17:24 +0430

    Add project files.

---
+++ Boxing And Unboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_And_Unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 22; // 22 will assigned to age
+            
+            Object Obj = age; // Boxing
+
+            age = 100;
+
+            System.Console.WriteLine("Value - type value of age is : {0}", age);
+            
+            System.Console.WriteLine("Object - type value of obj is : {0}", age);
+            
+            Console.ReadKey();
+        }
+    }
+}
